7c0ad6849cac3ffbfe7224a99e79d43d4e8447e3
Fernthedev <15272073+Fernthedev@users.noreply.github.com>
2021-08-13 11:21:10 -0400

    Update dependencies and fix C-ABI

--- build.rs
+++ build.rs
@@ -5,6 +5,9 @@ use std::env;
 fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
+    println!("Doing cdylib magic!");
+    cdylib_link_lines::metabuild();
+
     println!("Bindings generating");
     // cbindgen::generate(crate_dir)
     //     .expect("Unable to generate bindings")
